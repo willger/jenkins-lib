1598eab15f81740309560dbbade143601fa519f6
Ryan Willger <willger@gmail.com>
2020-12-14 13:28:12 -0600

    updated with slacksend

--- vars/stepUpdate.groovy
+++ vars/stepUpdate.groovy
@@ -1,7 +1,12 @@
 // vars/stepUpdate.groovy
 
-def call(input) {
-  sh "echo 'Echo: ${input}'"
+def call(sclr, smsg) {
+  if (binding.hasVariable('slackResponse')) {
+      slackSend(botUser: true, color: sclr, channel: ts_id, message: smsg, tokenCredentialId: 'Slack-Max-Token', iconEmoji: 'dog', username: 'Dommie')
+  } else {
+      slackResponse = slackSend(botUser: true, color: sclr, channel: '#pipeline', message: smsg, tokenCredentialId: 'Slack-Max-Token',  iconEmoji: 'dog', username: 'Dommie')
+      ts_id = slackResponse.threadId
+  }
 }
 
 return this
